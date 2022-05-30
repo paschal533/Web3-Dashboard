31a69c28b0022bedfeef166b49ea54d2b00b4e40
Paschal <58183764+paschal533@users.noreply.github.com>
2022-05-30 21:12:46 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { MoralisProvider } from "react-moralis";
+//import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
   return(
